de93500a53526e819b5a8497b22d5eea1977e5fb
Luana <goncalvesluana2107@icloud.com>
2025-10-22 14:21:45 -0300

    código final

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-
 import TodoForm from "./components/TodoForm";
 import TodoHeader from "./components/todoheader";
 import TodoList from "./components/TodoList";
 import { TodoContainer } from "./components/TodoContainer";
+import { useTodo } from "./hooks/useTodo";
 
 function App() {
+  const {
+    addTodo,
+    toggleTodoCompleted,
+    deleteTodo,
+    filteredTodos,
+    setFilter,
+    clearCompleted,
+    filter,
+  } = useTodo();
 
   return (
     <TodoContainer>
       <TodoHeader></TodoHeader>
 
-      <TodoForm></TodoForm>
+      <TodoForm addTodo={addTodo}></TodoForm>
 
-      <TodoList></TodoList>
-
-    </TodoContainer>     
+      <TodoList
+        todoList={filteredTodos}
+        toggleTodoCompleted={toggleTodoCompleted}
+        deleteTodo={deleteTodo}
+        setFilter={setFilter}
+        filter={filter}
+        clearCompleted={clearCompleted}
+      ></TodoList>
+    </TodoContainer>
   );
 }
 
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,34 +1,80 @@
 import { useContext } from "react";
 import { themeConfig } from "../../contexts/theme";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import type { Todo } from "../../hooks/useTodo";
+import IconCheck from "/images/icon-check.svg";
 
-const todos = [
-  { id: 1, text: "Todo 1" },
-  { id: 2, text: "Todo 2" },
-  { id: 3, text: "Todo 3" },
-];
+interface TodolistProps {
+  todoList: Todo[];
+  toggleTodoCompleted: (id: number) => void;
+  deleteTodo: (id: number) => void;
+  setFilter: (filter: "all" | "active" | "completed") => void;
+  filter: "all" | "active" | "completed";
+  clearCompleted: () => void;
+}
 
-const TodoList = () => {
+const TodoList = ({
+  todoList,
+  toggleTodoCompleted,
+  deleteTodo,
+  setFilter,
+  filter,
+  clearCompleted,
+}: TodolistProps) => {
   const { theme } = useContext(ThemeContext);
   return (
     <>
       <div className={`${themeConfig[theme].todo.backgroundColor} rounded-md`}>
         <ul>
-          {todos.map((todo) => (
+          {todoList.map((todo) => (
             <li
               className={`p-6 border-b ${themeConfig[theme].todo.borderColor}`}
               key={todo.id}
             >
-              <div className="flex items-center gap-4">
-                <span className="w-6 h-6 rounded-full hover:bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%)))] hover:p-[1px]">
-                  <button
-                    className={`w-full h-full border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer ${themeConfig[theme].todo.backgroundColor}`}
-                  ></button>
-                </span>
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-4">
+                  <span className="w-6 h-6 rounded-full hover:bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%)))] hover:p-[1px]">
+                    <button
+                      onClick={() => toggleTodoCompleted(todo.id)}
+                      className={`w-full h-full border ${
+                        themeConfig[theme].todo.borderColor
+                      } rounded-full cursor-pointer ${
+                        themeConfig[theme].todo.backgroundColor
+                      } ${
+                        todo.completed
+                          ? "bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%)))]"
+                          : ""
+                      }`}
+                    >
+                      {todo.completed && (
+                        <img
+                          src={IconCheck}
+                          alt="icone de check"
+                          className="h-2 w-2 m-auto"
+                        />
+                      )}
+                    </button>
+                  </span>
 
-                <p className={`${themeConfig[theme].todo.textColor}`}>
-                  {todo.text}
-                </p>
+                  <p
+                    className={`${themeConfig[theme].todo.textColor} ${
+                      todo.completed ? "line-through opacity-50" : ""
+                    }`}
+                  >
+                    {todo.text}
+                  </p>
+                </div>
+                <button
+                  onClick={() => deleteTodo(todo.id)}
+                  className={`
+                    ${themeConfig[theme].layout.textColor} ${
+                    theme === "dark"
+                      ? "hover:text-neutral-light-grayish-blue-hover"
+                      : "hover:text-neutral-very-dark-grayish-blue"
+                  } cursor-pointer`}
+                >
+                  X
+                </button>
               </div>
             </li>
           ))}
@@ -37,44 +83,79 @@ const TodoList = () => {
         <div
           className={`text-sm flex justify-between p-4 ${themeConfig[theme].layout.textColor}`}
         >
-          <p>{todos.length} items total</p>
-          
+          <p>{todoList.length} items total</p>
+
           <div className=" hidden sm:flex gap-4">
-            <button className={`text-check-blue cursor-pointer ${
-            theme === "dark"
-              ? "hover:text-neutral-light-grayish-blue-hover"
-              : "hover:text-neutral-very-dark-grayish-blue"
-          } `}>All</button>
-            <button className={`cursor-pointer ${
-            theme === "dark"
-              ? "hover:text-neutral-light-grayish-blue-hover"
-              : "hover:text-neutral-very-dark-grayish-blue"
-          } `}>Active</button>
-            <button className={`cursor-pointer ${
-            theme === "dark"
-              ? "hover:text-neutral-light-grayish-blue-hover"
-              : "hover:text-neutral-very-dark-grayish-blue"
-          } `}>Completed</button>
+            <button
+              onClick={() => setFilter("all")}
+              className={`${
+                filter === "all" ? `text-check-blue` : ""
+              } cursor-pointer ${
+                theme === "dark"
+                  ? "hover:text-neutral-light-grayish-blue-hover"
+                  : "hover:text-neutral-very-dark-grayish-blue"
+              } `}
+            >
+              All
+            </button>
+            <button
+              onClick={() => setFilter("active")}
+              className={`${
+                filter === "active" ? `text-check-blue` : ""
+              } cursor-pointer ${
+                theme === "dark"
+                  ? "hover:text-neutral-light-grayish-blue-hover"
+                  : "hover:text-neutral-very-dark-grayish-blue"
+              } `}
+            >
+              Active
+            </button>
+            <button
+              onClick={() => setFilter("completed")}
+              className={`${
+                filter === "completed" ? `text-check-blue` : ""
+              } cursor-pointer ${
+                theme === "dark"
+                  ? "hover:text-neutral-light-grayish-blue-hover"
+                  : "hover:text-neutral-very-dark-grayish-blue"
+              } `}
+            >
+              Completed
+            </button>
           </div>
 
-          <button className= {`cursor-pointer ${
-            theme === "dark"
-              ? "hover:text-neutral-light-grayish-blue-hover"
-              : "hover:text-neutral-very-dark-grayish-blue"
-          } `}>Clear Completed</button>
+          <button
+            onClick={clearCompleted}
+            className={`cursor-pointer ${
+              theme === "dark"
+                ? "hover:text-neutral-light-grayish-blue-hover"
+                : "hover:text-neutral-very-dark-grayish-blue"
+            } `}
+          >
+            Clear Completed
+          </button>
         </div>
       </div>
-
       <div
         className={`${themeConfig[theme].todo.backgroundColor}  ${themeConfig[theme].layout.textColor} flex justify-center gap-5 py-4 rounded-md mt-4 sm:hidden`}
       >
-        <a className={`text-check-blue cursor-pointer ${
+        <button
+          onClick={() => setFilter("all")}
+          className={`${
+            filter === "all" ? `text-check-blue` : ""
+          } cursor-pointer ${
             theme === "dark"
               ? "hover:text-neutral-light-grayish-blue-hover"
               : "hover:text-neutral-very-dark-grayish-blue"
-          } `}>All</a>
+          } `}
+        >
+          All
+        </button>
         <button
-          className={`cursor-pointer ${
+          onClick={() => setFilter("active")}
+          className={`${
+            filter === "active" ? `text-check-blue` : ""
+          } cursor-pointer ${
             theme === "dark"
               ? "hover:text-neutral-light-grayish-blue-hover"
               : "hover:text-neutral-very-dark-grayish-blue"
@@ -82,11 +163,18 @@ const TodoList = () => {
         >
           Active
         </button>
-        <button className={`cursor-pointer ${
+        <button
+          onClick={() => setFilter("completed")}
+          className={`${
+            filter === "completed" ? `text-check-blue` : ""
+          } cursor-pointer ${
             theme === "dark"
               ? "hover:text-neutral-light-grayish-blue-hover"
               : "hover:text-neutral-very-dark-grayish-blue"
-          } `}>Completed</button>
+          } `}
+        >
+          Completed
+        </button>
       </div>
     </>
   );
